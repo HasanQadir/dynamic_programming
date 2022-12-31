c7789c5bb2899dbc12a1af6b3283542f261de4d8
Hasan Qadir <hasan.qadir@gmail.com>
2022-12-31 19:23:44 +0500

    Fibonacci Series, memoization solution

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,22 @@
-def fib(n):
+def recursive_fib(n):
     if n ==1 or n ==2:
         return 1
     else:
-        return fib(n-1) + fib(n-2)
+        return recursive_fib(n-1) + recursive_fib(n-2)
+
+def dp_memoize_fib(n, arr):
+    if arr[n] != None:
+        result = arr[n]
+    if n ==1 or n ==2:
+        return 1
+    else:
+        result = dp_memoize_fib(n-1, arr) + dp_memoize_fib(n-2, arr)
+    return result
 
 def main():
     n = int(input("Enter fibonacci number: "))
-    result = fib(n)
+    # result = recursive_fib(n)
+    result = dp_memoize_fib(n, [None] * (n + 1))
     print("Result:", result)
 
 if __name__ == "__main__":
