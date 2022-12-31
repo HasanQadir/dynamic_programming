1477792a869272309fdba870dd67ce9926564a48
Hasan Qadir <hasan.qadir@gmail.com>
2023-01-01 00:05:40 +0500

    Fibonacci Series, bottomup solution

--- fibonacci.py
+++ fibonacci.py
@@ -8,15 +8,26 @@ def dp_memoize_fib(n, arr):
     if arr[n] != None:
         result = arr[n]
     if n ==1 or n ==2:
-        return 1
+        result = 1
     else:
         result = dp_memoize_fib(n-1, arr) + dp_memoize_fib(n-2, arr)
     return result
 
+def dp_bottomup_fib(n):
+    if n ==1 or n ==2:
+        return 1
+    bottom_up = [0] * (n)
+    bottom_up[0] = 1
+    bottom_up[1] = 1
+    for i in range(2,n):
+        bottom_up[i] = bottom_up[i-1] + bottom_up[i-2]
+    return bottom_up[n-1]
+
 def main():
     n = int(input("Enter fibonacci number: "))
     # result = recursive_fib(n)
-    result = dp_memoize_fib(n, [None] * (n + 1))
+    # result = dp_memoize_fib(n, [None] * (n + 1))
+    result = dp_bottomup_fib(n)
     print("Result:", result)
 
 if __name__ == "__main__":
