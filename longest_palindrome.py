8194a73b6bc861b06be37d1e611e1237a00ac167
Hasan Qadir <hasan.qadir@gmail.com>
2023-01-03 10:39:49 +0500

    Longest palindrome problem - 1st iteration

--- longest_palindrome.py
+++ longest_palindrome.py
@@ -1,7 +1,24 @@
-class Solution(object):
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        for i in range(len(s)):
+def longestPalindrome(s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    list_str = []
+    result = ''
+    for i in range(len(s)-1, 0, -1):
+        list_str.append(s[:i])
+        print(list_str)
+        for item in list_str:
+            _str = item
+            if _str[0:] == _str[::-1]:
+                if len(_str) >= len(result):
+                    result = _str
+    return result
+
+def main():
+    _str = str(input("Enter string: "))
+    result = longestPalindrome(_str)
+    print("Longest Palindrome:", result)
+
+if __name__ == "__main__":
+    main()
